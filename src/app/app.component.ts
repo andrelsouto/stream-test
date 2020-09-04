b48c2f6876b588a046ae626d2386cc5a147c7d96
Andre Luiz <andre.andrade@vsoft.com.br>
2020-09-04 13:11:38 -0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { initStream, getStreams, closeStream, takeSnapshot, watchStream } from '@vsoft/middleware-easy-proctor'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-teste';
+  //MVP
+  token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjAiLCJ1bmlxdWVfbmFtZSI6IlZzb2Z0Iiwicm9sZSI6ImFkbWluIiwibmJmIjoxNTk5MTQ5ODI3LCJleHAiOjE1OTkxNDk4ODcsImlhdCI6MTU5OTE0OTgyN30.gf9RMh7QzMKgXWdDmBbfezigGYJ1AnHmu2Qilj9CUd0';
+  //DEV
+  //token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjEiLCJ1bmlxdWVfbmFtZSI6InN0cmluZyIsInJvbGUiOiJhZG1pbiIsIm5iZiI6MTU5NzQzMDE4NSwiZXhwIjoxNTk3NDMwMjQ1LCJpYXQiOjE1OTc0MzAxODV9.mTnLotnK72r9V-DAtL-aMAFpqwM-WA56xy1-k0QBios'
+  streams = [];
+  init = false;
+
+   async startStream(){
+     this.init = true;
+    await initStream(this.token, '1-10065578777');
+    this.init = false;
+  }
+
+  async subscribeStream() {
+    await watchStream(this.token, this.streams[0], 'video-stream');
+  }
+
+  async getStream() {
+    this.streams = await getStreams(this.token);
+    console.log(this.streams);
+  }
+
+  finishStream() {
+    closeStream();
+  }
+
+  snapshot() {
+    const t = takeSnapshot();
+    console.log(t);
+  }
+
 }
